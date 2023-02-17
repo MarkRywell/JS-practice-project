baa459defb20a86c1c57783aca828e09c11b0cc3
Mark Rywell Gaje <gopro855@gmail.com>
2023-02-17 21:14:42 +0800

    Simple OOP implementation

--- JS Codes/OOP/person.js
+++ JS Codes/OOP/person.js
@@ -1,3 +1,18 @@
 class Person {
-    
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    walk() {
+        return `${this.name} is now walking.`;
+    }
+
+    stand() {
+        return `${this.name} is now standing`;
+    }
 }
+
+let person = new Person("Mark", 21);
+
+console.log(person.walk());
