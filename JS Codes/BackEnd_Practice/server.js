99b0bc843f9dd0e353822baec717e1136788ab43
Mark Rywell Gaje <gopro855@gmail.com>
2023-02-27 21:44:09 +0800

    Migrate from mongodb to mongoose

--- JS Codes/BackEnd_Practice/server.js
+++ JS Codes/BackEnd_Practice/server.js
@@ -1,31 +1,56 @@
-const express = require('express');
 const { dbConnect, getDb } = require('./db');
+require('dotenv').config();
+const express = require('express');
+const foodsRoute = require('./routes/foods');
 
+
+// Express App
 const app = express();
 
+// database
 let db;
 
-dbConnect((error) => {
-    if(!error) {
-        app.listen(3000, () => {
-            console.log('App running at PORT 3000');
-        });
 
-        db = getDb();
-    }
+// Middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 });
 
-app.get('/members', (req, res) => {
-    let members = [];
-    
-    db.collection('members')
-    .find()
-    .forEach(member => members.push(member))
-    .then(() => {
-        res.status(200).json(members);
-    })
-    .catch(() => {
-        res.status(500).json({error: "Error fetching data from the Database"});
-    });
 
+// Routes
+app.use('/api/foods', foodsRoute);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`App running at PORT ${process.env.PORT}`);
 });
+
+
+// dbConnect((error) => {
+    // if(!error) {
+        // app.listen(3000, () => {
+            // console.log('App running at PORT 3000');
+        // });
+// 
+        // db = getDb();
+    // }
+// });
+
+
+
+// app.get('/members', (req, res) => {
+    // let members = [];
+    // 
+    // db.collection('members')
+    // .find()
+    // .forEach(member => members.push(member))
+    // .then(() => {
+        // res.status(200).json(members);
+    // })
+    // .catch(() => {
+        // res.status(500).json({error: "Error fetching data from the Database"});
+    // });
+// 
+// });
+// 
