b6e1606ed1f5e892aff0adacbff4d579a3e7b7ed
Mark Rywell Gaje <gopro855@gmail.com>
2023-02-28 10:50:19 +0800

    Updated Routes and Added Model

--- JS Codes/BackEnd_Practice/routes/foods.js
+++ JS Codes/BackEnd_Practice/routes/foods.js
@@ -1,20 +1,32 @@
 const express = require('express');
+const Food = require('../models/Food');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
 
-    const page = req.query.page || 0;
-    const dataPerPage = 5;
-    let foods = [];
+    // const page = req.query.page || 0;
+    // const dataPerPage = 5;
+    // let foods = [];
 
-    db.collection('foods')
-    .find()
-    .skip(page * dataPerPage)
-    .limit(dataPerPage)
-    .forEach(food => foods.push(food))
-    .then(() => {res.status(200).json(foods)})
-    .catch(error => {res.status(500).json(error)});
+    // db.collection('foods')
+    // .find()
+    // .skip(page * dataPerPage)
+    // .limit(dataPerPage)
+    // .forEach(food => foods.push(food))
+    // .then(() => {res.status(200).json(foods)})
+    // .catch(error => {res.status(500).json(error)});
+
+    try {
+        let foods = [];
+        foods = await Food.find()
+        res.status(200).json(foods);
+    }
+    catch (error) {
+        res.status(500).json({error: error.message});
+    }
+
+
 
 });
 
@@ -33,8 +45,14 @@ router.get('/:name', (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    try {
+        const food = await Food.create(req.body);
+        res.status(201).json(food);
+    }
+    catch (error) {
+        res.status(500).json({error: error.message});
+    }
 });
 
 router.get('/wtf', (req, res) => {
